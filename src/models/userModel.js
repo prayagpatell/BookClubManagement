44c52a11a7e4386ae7c72dbd022acacd72f28b21
katelyn3212 <kchees38@gmail.com>
2024-10-15 21:51:15 -0400

    models

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,28 @@
-import 
+import { Sequelize, DataTypes } from 'sequelize';
+import sequelize from '../db.js';
+
+const User = sequelize.define('User', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  club_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Clubs', // Name of the table
+      key: 'club_id'  // Key in the Clubs table that we're referencing
+    }
+  }
+});
+
+export default User;
